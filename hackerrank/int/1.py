836cc67a900cee090e0064a2e0b2fb82d92a4caf
t-xim <46313127+t-xim@users.noreply.github.com>
2020-03-30 16:17:55 +1100

    Create 1.py

---
+++ hackerrank/int/1.py
@@ -0,0 +1,12 @@
+# Mud and Wall
+# One unit width of gap between walls will contain one segment of packed raw material
+# The height of the raw material cannot exceed 1 unit more than adjacent wall
+# E.g. 
+# wallPos = [1, 5, 7]
+# wallHeight = [4, 6, 8]
+# Input
+# 3 (this is n)
+# 1 6 9 (wallPos)
+# 4 3 3 (wallHeight)
+# Output
+# 6 (maximum wall of segment)
