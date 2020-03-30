acb62dd46ab4b680d89ac97a03b940a3b7c023ee
t-xim <46313127+t-xim@users.noreply.github.com>
2020-03-30 16:24:55 +1100

    Create 3.py

---
+++ hackerrank/int/3.py
@@ -0,0 +1,14 @@
+# nodes and edges
+# Find minimum number of steps to connect all nodes
+# Given:
+# totalNodes
+# totalEdges
+# edge numbers
+# E.g:
+# 5 4 (totalNodes, totalEdges)
+# 1 2 (edge)
+# 2 3 (edge)
+# 3 4 (edge)
+# 4 1 (edge)
+# 1 step needed
+# -1 if not possible
